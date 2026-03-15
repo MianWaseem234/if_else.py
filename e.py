386d48d89622411d362ead68f18c1ee72c0c3268
MianWaseem234 <tasawafeahsaan@gmail.com>
2026-03-15 04:31:48 -0700

    Add files via upload

---
+++ e.py
@@ -0,0 +1,14 @@
+def check_num(num):
+    if num % 2==0:
+        print("even")
+
+    else:
+        print("odd")
+
+check_num(6)
+
+check_num(9)
+check_num(8)
+check_num(00)
+check_num(200)
+check_num(111)
